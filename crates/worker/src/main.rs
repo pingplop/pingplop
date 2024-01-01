817213bad03d9097ff4b84b0d290bf23b9c71e3a
Aris Ripandi <riipandi@gmail.com>
2024-01-01 23:40:57 +0700

    feat: initialize worker package

---
+++ crates/worker/src/main.rs
@@ -0,0 +1,9 @@
+// Copyright 2023-current Aris Ripandi <pingplop@skiff.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    println!("Not yet implemented!");
+
+    Ok(())
+}
